4c3b120fe9a961b754672075365081a3aecac9dd
Diacius <77890144+Diacius@users.noreply.github.com>
2024-06-23 16:37:26 +0100

    working API SLAYYYYYY

--- server/main.py
+++ server/main.py
@@ -1,17 +1,26 @@
 from flask import Flask, request, Response
-import sqlite3
+from flask_cors import CORS
+import sqlite3, json
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/api/bookinfo', methods=['GET'])
+@app.route('/bookinfo')
 def bookinfo():
     con = sqlite3.connect("demodb.sqlite")
     cur = con.cursor()
-    error = None
-    request.form['barcode']
+    query = cur.execute("SELECT * FROM books WHERE barcode =" + request.args.get("barcode"))
+    book = query.fetchone()
+    con.close()
+    return Response(json.dumps(book), status=200)
+@app.route('/bookmoreinfo')
+def bookmoreinfo():
     con = sqlite3.connect("demodb.sqlite")
     cur = con.cursor()
+    query = cur.execute("SELECT * FROM bookinfo WHERE barcode =" + request.args.get("barcode"))
+    book = query.fetchone()
+    con.close()
+    return Response(json.dumps(book), status=200)
--- web/js/bookinfo.js
+++ web/js/bookinfo.js
@@ -9,34 +9,57 @@ form.addEventListener("submit", (event) => {
     
     let formData = new FormData(form);
     barcode = formData.get("barcode")
-    bookData = books[barcode]
-    if (bookData) {
-    isbn = bookData["isbn"]
-    title = bookData["title"]
-    author = bookData["author"];
-    outputTable = document.querySelector("#outputTable").querySelector('#tbody')
-    row = outputTable.insertRow()
-    barcodeCell = row.insertCell(0);
-    barcodeCell.innerHTML = barcode;
-    isbnCell = row.insertCell(1);
-    isbnCell.innerHTML = isbn;
-    titleCell = row.insertCell(2);
-    titleCell.innerHTML = title;
-    authorCell = row.insertCell(3);
-    authorCell.innerHTML = author;
-    statusOutput.innerHTML = `<div class="alert alert-success" role="alert">Book found: ${title} by ${author}</div>`
-    moreInfoCell= row.insertCell(4);
-    moreInfoCell.innerHTML = `<a href="javascript:moreInfo(${barcode})">More Info</a>`
-    }
-    else {
-        statusOutput.innerHTML= `<div class="alert alert-danger" role="alert">Lookup Failed: Barcode did not match item in database. Try rescanning the book.</div>`
-    }
+    var xhr = new XMLHttpRequest();
+    url = "http://127.0.0.1:5000/bookinfo?barcode=" + barcode
+    xhr.open("GET", url, true)
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            bookData = JSON.parse(xhr.response);
+            if (bookData) {
+                isbn = bookData[1]
+                title = bookData[2]
+                author = bookData[3];
+                outputTable = document.querySelector("#outputTable").querySelector('#tbody')
+                row = outputTable.insertRow()
+                barcodeCell = row.insertCell(0);
+                barcodeCell.innerHTML = barcode;
+                isbnCell = row.insertCell(1);
+                isbnCell.innerHTML = isbn;
+                titleCell = row.insertCell(2);
+                titleCell.innerHTML = title;
+                authorCell = row.insertCell(3);
+                authorCell.innerHTML = author;
+                statusOutput.innerHTML = `<div class="alert alert-success" role="alert">Book found: ${title} by ${author}</div>`
+                moreInfoCell= row.insertCell(4);
+                moreInfoCell.innerHTML = `<a href="javascript:moreInfo(${barcode})">More Info</a>`
+                }
+                else {
+                    statusOutput.innerHTML= `<div class="alert alert-danger" role="alert">Lookup Failed: Barcode did not match item in database. Try rescanning the book.</div>`
+                }
+        }
+      };
+    
   });
 function moreInfo(barcode) {
-    bookData = books[barcode]
+    
     let moreInfoModal = new bootstrap.Modal(document.getElementById('moreInfoModal'))
     let moreInfoTitle = document.getElementById('moreInfoTitle');
+    let moreInfoBody = document.getElementById('moreinfobody')
+    var xhr = new XMLHttpRequest();
+    url = "http://127.0.0.1:5000/bookinfo?barcode=" + barcode
+    xhr.open("GET", url, true)
+    xhr.send();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            bookData = JSON.parse(xhr.response);
+            if (bookData) {
+                isbn = bookData[1]
+                title = bookData[2]
+                author = bookData[3];
+            }}}
     moreInfoTitle.innerHTML = `More info about "${bookData["title"]}" by "${bookData["author"]}"`
+    
     moreInfoModal.show()
 
 
